47c4a4a2120ce02a6de542d5e0611769d4ad388e
FelipeCardos <felcarbib@gmail.com>
2025-02-22 09:23:27 +0000

    Start of Hylle application with a working version of swagger ui

---
+++ hylle/src/main/java/com/hylle/HylleApplication.java
@@ -0,0 +1,13 @@
+package com.hylle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HylleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HylleApplication.class, args);
+	}
+
+}
---
+++ hylle/src/test/java/com/hylle/hylle/HylleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hylle.hylle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HylleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
